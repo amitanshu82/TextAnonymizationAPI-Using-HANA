0708867b76e7f840f03126a3378741626acff509
I307170 <I307170@INLN50938913A.apj.global.corp.sap>
2018-06-15 15:13:27 +0530

    Commit for new controller code . DBservice code is transfered to Controller only

--- i307170-model/TextAnonymizationAPI/src/main/java/io/poc/text/anym/app/controller/TextAnonymizationApiAppController.java
+++ i307170-model/TextAnonymizationAPI/src/main/java/io/poc/text/anym/app/controller/TextAnonymizationApiAppController.java
@@ -1,8 +1,12 @@
 package io.poc.text.anym.app.controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sql.DataSource;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +21,17 @@ import io.poc.text.anym.app.entity.TextInput;
 
 @RestController
 public class TextAnonymizationApiAppController {
+	
+	static String hanaHost = "localhost";
+	static int hanaPort = 30015;
+	static String hanaUser = "SBSS_49981159700204747010855902398847012696392672220285900501933237456";
+	static String hanaPassword = "Wk7A8QQiB6cB3XLHDo_.C-agqyeI15HKdYtZm0tKOd55BI4fIel_iQgvpiTlUXVq7xDBlQwBqMgPcsb4CwO1NbCse-DBRPTEqmPARVlg8EKv2Uouid.nhk9OZlFrF0Fo";
+	static String hanaSchema = "DLP";
+	static int queryResult = 0;
+	static DataSource ds = null;
+	static ResultSet resultSetData = null;
+
+	static int maxID = 0;
 	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	
@@ -37,6 +52,9 @@ public class TextAnonymizationApiAppController {
 		List<TextAnonym> textanonym = new ArrayList<TextAnonym>();
 		int strlen = text_input.length();
 		int maxid = io.poc.text.anym.dbservices.HdbServices.getMaxId();
+		int id = maxid + 1;
+		ResultSet resultSet1 = null;
+		Connection connection = null;
 		if(strlen <= 5000)
 		{
 			textin.setId(maxid + 1);
@@ -44,9 +62,52 @@ public class TextAnonymizationApiAppController {
 		 	textinput.add(textin);	 	
 		}
 		
-		int rows = io.poc.text.anym.dbservices.HdbServices.insertData(textinput);
-		if (rows != 0)
-		textanonym = io.poc.text.anym.dbservices.HdbServices.getData(maxid + 1);
+		   io.poc.text.anym.dbservices.HdbServices.insertData(textinput);
+
+		try
+		{		       
+		       if (ds == null){
+		          ds = io.poc.text.anym.dbservices.HANADataSourceCreator.createHanaDataSource(hanaHost, hanaPort, hanaUser, hanaPassword, hanaSchema);
+		          if (ds != null)
+		          System.out.println("Data Source Created for CF DB connection.");
+		          
+		          else
+		          System.out.println("Data Source not Created for CF DB connection.");
+		        }
+		       connection = ds.getConnection();
+		       if(connection != null)
+		         System.out.println("Connection to DB successful...");
+		       else System.out.println("Connection to DB is not successful...");		 
+		       Statement stmt = connection.createStatement();
+			   String sqlquery = "SELECT * FROM \"DLP\".\"$TA_TestHana.HDBModule::EXT_Core.hdbfulltextindex\" where ID = " + id +"  AND TA_TYPE IN ( 'PERSON', 'COUNTRY', 'EMPLOYEE_ID','URI/EMAIL', 'URI/URL', 'ORGANIZATION', 'CURRENCY', 'PHONE' )  ";
+			   System.out.println("Query that is fired "+sqlquery);
+			   resultSet1 = stmt.executeQuery(sqlquery);
+			   stmt.close();
+			   
+			}
+			catch(Exception e) {
+			 }finally {
+		            if (connection != null) {
+		                try {
+		                	connection.close();
+		                } catch (SQLException e) {}
+		            }
+		     }
+		    try {
+		    if (resultSet1 != null){
+				while(resultSet1.next()){
+				    TextAnonym txtanym = new TextAnonym();
+				    txtanym.setTa_token(resultSet1.getNString("TA_TOKEN"));
+				    txtanym.setTa_type(resultSet1.getString("TA_TYPE"));
+				    textanonym.add(txtanym);
+				}
+				
+					resultSet1.close();
+		    }
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		
 		return textanonym; 
 	  }
